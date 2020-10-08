544b6017012087d6253e4909055bd9e9db49aa36
Hsu You-Ming <youming.hsu@ace.io>
2020-10-08 14:24:27 +0800

    Add getSecureInt api

--- src/math/random/index.js
+++ src/math/random/index.js
@@ -1,5 +1,8 @@
+'use strict';
+
 const assert = require('assert');
-const createHash = require('create-hash');
+const BN = require('bn.js');
+const crypto = require('crypto');
 
 const getFloat = (min, max) => {
   assert(min == +min, 'Please enter at least one number.');
@@ -27,12 +30,55 @@ const getBool = () => {
 
 const getSha256 = () => {
   let seed = (Math.random() / Date.now()).toString(36);
-  return createHash('sha256').update(seed).digest().toString('hex');
+  return crypto.createHash('sha256').update(seed).digest('hex');
+}
+
+const getSecureInt = (minimum, maximum) => {
+  if (maximum == null) {
+    maximum = minimum;
+    minimum = 0;
+  }
+  if (!/^[0-9]+$/.test(minimum)) {
+    throw new Error('The minimum value must be an integer.');
+  }
+  if (!/^[0-9]+$/.test(maximum)) {
+    throw new Error('The maximum value must be an integer.');
+  }
+  const min = new BN(minimum);
+  const max = new BN(maximum);
+  if (max.lt(min)) {
+    throw new Error('The maximum value must be higher than the minimum value.');
+  }
+  const zero = new BN(0), one = new BN(1), eight = new BN(8);
+  let range = max.sub(min);
+	let bitsNeeded = zero;
+  let bytesNeeded = zero;
+  let mask = one;
+	while (range.gt(zero)) {
+		if (bitsNeeded.mod(eight).eq(zero)) {
+			bytesNeeded = bytesNeeded.add(one);
+		}
+    bitsNeeded = bitsNeeded.add(one);
+    mask = mask.shln(1).or(one);
+		range = range.ushrn(1);
+  }
+  const randomBytes = crypto.randomBytes(+bytesNeeded);
+  let randomValue = zero;
+  for (let i = 0; i < +bytesNeeded; i++) {
+    randomValue = randomValue.or(new BN(randomBytes[i]).shln(8 * i));
+  }
+  randomValue = randomValue.and(mask);
+  if (randomValue.lte(max.sub(min))) {
+    return min.add(randomValue).toString();
+  } else {
+    return getSecureInt(minimum, maximum);
+  }
 }
 
 module.exports = {
   getFloat,
   getInt,
+  getSecureInt,
   getBool,
   getSha256
 };
